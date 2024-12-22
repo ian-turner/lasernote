a4056b81376b654e8ea4318dea10ec409b09b665
ian-turner <i.r.turner55@gmail.com>
2024-12-22 16:40:13 -0500

    implemented basic editor

--- frontend/src/components/NoteEditor.js
+++ frontend/src/components/NoteEditor.js
@@ -1,13 +1,23 @@
-import { MDXEditor, headingsPlugin } from '@mdxeditor/editor';
+import {
+    MDXEditor,
+    headingsPlugin,
+    toolbarPlugin,
+    KitchenSinkToolbar,
+} from '@mdxeditor/editor';
 
 
 export default function Editor({ markdown, editorRef }) {
     return (
-        <MDXEditor
-            onChange={e => console.log(e)}
-            ref={editorRef}
-            markdown={markdown}
-            plugins={[headingsPlugin()]}
-        />
+        <div className='bg-light w-100'>
+            <MDXEditor
+                onChange={e => console.log(e)}
+                ref={editorRef}
+                markdown={markdown}
+                plugins={[
+                    headingsPlugin(),
+                    toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar/> }),
+                ]}
+            />
+        </div>
     );
 }
