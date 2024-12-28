071e4cc1ac2a6bcf5952067ce321af4059ccd849
ian-turner <i.r.turner55@gmail.com>
2024-12-27 22:11:50 -0500

    implemented register

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,94 @@
-import pymongo
+import hashlib
+import uuid
+import secrets
+from pymongo import MongoClient
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 
+TOKEN_SIZE_BYTES = 32
+
+client = MongoClient('mongo', 27017)
+db = client.appdb
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+
+def login_required(func):
+    def func_wrapper(*args, **kwargs):
+        auth_token = request.cookies.get('auth_token')
+        current_user = db.get_token_user(auth_token)
+        if not current_user:
+            return make_response({'message': 'Invalid credentials'}, 400)
+
+        return func(current_user, *args, **kwargs)
+
+    func_wrapper.__name__ = func.__name__
+    return func_wrapper
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    # getting user data
+    data = request.json
+    if 'email' not in data or 'password' not in data:
+        return make_response({'message': 'Invalid input'}, 400)
+
+    email = data['email']
+    password = data['password']
+
+    # checking if existing user
+    existing = db.users.find_one({'email': email})
+    if existing:
+        return make_response({'message': 'Existing user with that email address'}, 400)
+
+    # hashing password
+    _hash = hashlib.sha256()
+    _hash.update(password.encode())
+    hashed_password = _hash.hexdigest()
+
+    # creating authentication token
+    auth_token = secrets.token_hex(TOKEN_SIZE_BYTES)
+
+    # inserting into database
+    db.users.insert_one({'email': email, 'hashed_password': hashed_password, 'auth_tokens': [auth_token]})
+
+    # return results to user
+    res = make_response({'message': 'User registered'})
+    res.set_cookie('auth_token', auth_token)
+    return res
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    # getting user data
+    data = request.json
+    if 'email' not in data or 'password' not in data:
+        return make_response({'message': 'Invalid input'}, 400)
+
+    email = data['email']
+    password = data['password']
+
+    # hashing password
+    _hash = hashlib.sha256()
+    _hash.update(password.encode())
+    hashed_password = _hash.hexdigest()
+
+    # checking against database
+    user = db.users.find_one({'email': email, 'hashed_password': hashed_password})
+    if not user:
+        return make_response({'message': 'Invalid login credentials'}, 400)
+
+    # creating authentication token
+    auth_token = secrets.token_hex(TOKEN_SIZE_BYTES)
+
+    # inserting into database
+    db.users.update_one({'email': user['email']}, {'$push': {'auth_tokens': auth_token}})
+
+    # return results to user
+    res = make_response({'message': 'User authenticated'})
+    res.set_cookie('auth_token', auth_token)
+    return res
+
 
 @app.route('/')
 def home():
---
+++ frontend/src/app/register/page.js
@@ -0,0 +1,62 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+
+export default function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const res = await fetch('http://localhost:5000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        if (res.ok) {
+            router.push('/app');
+        }
+        else {
+            // handle error here
+        }
+    }
+
+    return (
+        <div
+            className='d-flex justify-content-center align-items-center'
+            style={{
+                width: '100vw',
+                height: '100vh',
+            }}>
+            <form
+                className='p-5 container form-control bg-light'
+                style={{ maxWidth: '400px' }}
+                onSubmit={handleSubmit}>
+                <h1 className='h1 mb-4 text-center'>Register</h1>
+                <div className='mb-4'>
+                    <label>Email</label>
+                    <input
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className='form-control'
+                        type='email'
+                        placeholder='EMAIL'/>
+                </div>
+                <div className='mb-4'>
+                    <label>Password</label>
+                    <input
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        className='form-control'
+                        type='password'
+                        placeholder='PASSWORD'/>
+                </div>
+                <button className='btn btn-primary w-100' type='submit'>Register</button>
+            </form>
+        </div>
+    );
+}
